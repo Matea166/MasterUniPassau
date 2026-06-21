77cc8e898500e6456adfa0b6963f14cb43409cfb
Matea Qazolli <116714646+Matea166@users.noreply.github.com>
2026-06-21 22:38:07 +0200

    Update shebang and read command in script

--- bnsl-qa/qerror-graph-generation.sh
+++ bnsl-qa/qerror-graph-generation.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-BASE_DIR="./dispatch_output/solver_outputs"
 OUTPUT_BASE="q-error_output"
 CARD_RESULTS_DIR="../bnsl/card_results"
 
@@ -46,7 +45,7 @@ while true; do
     done
 
     echo "Add another SA/SQA pair? (y/n)"
-    read add_more
+    read -r add_more
     if [[ "$add_more" != "y" ]]; then
         break
     fi
