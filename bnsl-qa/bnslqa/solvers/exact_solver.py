d194eaebd41fbac15b945fee544c26393b565450
Matea Qazolli <116714646+Matea166@users.noreply.github.com>
2026-06-28 00:51:48 +0200

    Update exact_solver.py

--- bnsl-qa/bnslqa/solvers/exact_solver.py
+++ bnsl-qa/bnslqa/solvers/exact_solver.py
@@ -1,3 +1,6 @@
+# This file is based on code from BNSL-QA-PYTHON.
+# Original project licensed under the GNU General Public License, Version 2.
+
 from time import time_ns
 import torch
 from bnslqa.solvers.qubo_matrix import calcQUBOMatrix
