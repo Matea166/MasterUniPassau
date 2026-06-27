a890259daef2b7afd0c3b6c3b571f73a76f0f2f7
Matea Qazolli <116714646+Matea166@users.noreply.github.com>
2026-06-28 01:07:04 +0200

    Update null.py

--- bnsl/tldks_2020/null.py
+++ bnsl/tldks_2020/null.py
@@ -1,3 +1,6 @@
+# This file is based on code from tldks-2020 by Max Halford.
+# Original project licensed under the Apache License, Version 2.0.
+
 class Null():
     """Missing values are a pain to work with. This class is here to remove
     some of the pain.
