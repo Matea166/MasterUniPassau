4d557b3d8f9b011ad1dffcbf4bcf4c18df1035ca
Matea Qazolli <116714646+Matea166@users.noreply.github.com>
2026-06-27 13:38:30 +0200

    Change nhanes_data columns to DOUBLE PRECISION

--- docker/postgres/init/01-init-nhanes-and-imdb.sql
+++ docker/postgres/init/01-init-nhanes-and-imdb.sql
@@ -6,14 +6,14 @@ CREATE DATABASE imdb;
 DROP TABLE IF EXISTS nhanes_data;
 
 CREATE TABLE nhanes_data (
-    seqn INTEGER,
+    seqn DOUBLE PRECISION,
     age_group TEXT,
-    ridageyr INTEGER,
-    riagendr INTEGER,
-    paq605 INTEGER,
+    ridageyr DOUBLE PRECISION,
+    riagendr DOUBLE PRECISION,
+    paq605 DOUBLE PRECISION,
     bmxbmi DOUBLE PRECISION,
     lbxglu DOUBLE PRECISION,
-    diq010 INTEGER,
+    diq010 DOUBLE PRECISION,
     lbxglt DOUBLE PRECISION,
     lbxin DOUBLE PRECISION
 );
