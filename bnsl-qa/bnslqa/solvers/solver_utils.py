0dfe382ec90baf6689448deb0bfc5f660a201cb3
Matea Qazolli <116714646+Matea166@users.noreply.github.com>
2026-06-28 00:57:47 +0200

    Add licensing comments to solver_utils.py
    Add licensing information at the top of the solver_utils.py file.

--- bnsl-qa/bnslqa/solvers/solver_utils.py
+++ bnsl-qa/bnslqa/solvers/solver_utils.py
@@ -1,3 +1,6 @@
+# This file is based on code from BNSL-QA-PYTHON.
+# Original project licensed under the GNU General Public License, Version 2.
+
 import torch
 
 def getExpectedSolution(solution, Q, indexQUBO, posOfIndex, n):
